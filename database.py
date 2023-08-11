f66ddc8651325c58d8d9dc237e604ebd847b7327
cp-james-harbeck <jamesharbeck@cipherproxy.com>
2023-08-11 01:47:25 -0400

    continue updating server for heroku deploy

--- database.py
+++ database.py
@@ -46,7 +46,7 @@ def get_dream(dream_id):
 
 def get_dreams(userEmail):
     log("Fetching all dreams.", type="info")
-    memories = get_memories("dreams")
+    memories = get_memories("dreams", n_results=2222)
     dreams = [
         {
             "id": memory["id"],
